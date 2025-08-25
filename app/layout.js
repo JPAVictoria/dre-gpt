4ba2ad5540c57e7e218f763aff7f4f2bf5b54319
Dre <92935512+JPAVictoria@users.noreply.github.com>
2025-08-25 20:41:50 +0800

    add favicon, cleanup files, apply analytics

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -8,13 +9,37 @@ const inter = Inter({
 
 export const metadata = {
   title: 'DreGPT',
-  description: "Simple yet sharp chatbot powered by Next.js and Google's Gemini AI"
+  description: "A simple chatbot powered by Next.js and Google's Gemini AI",
+  keywords: ['Next.js', 'AI', 'Chatbot', 'Gemini', 'DreGPT'],
+  authors: [{ name: 'Dre' }],
+  openGraph: {
+    title: 'DreGPT - Chatbot',
+    description: "A simple chatbot powered by Next.js and Google's Gemini AI",
+    url: 'https://dre-gpt.vercel.app',
+    siteName: 'DreGPT',
+    images: [
+      {
+        url: '/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'DreGPT Chatbot'
+      }
+    ],
+    locale: 'en_US',
+    type: 'website'
+  },
+  icons: {
+    icon: '/favicon.ico'
+  }
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className={`${inter.variable} antialiased`}>{children}</body>
+      <body className={`${inter.variable} antialiased`}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
